adbf46fd9baf191ac57b750d287ae53497252d44
sanket-godse <sanketgodse059@gmail.com>
2022-02-25 22:14:14 +0530

    commit 4

--- list.py
+++ list.py
@@ -1,3 +1,9 @@
+"""A list is a data structure in Python that is a mutable, 
+or changeable, ordered sequence of elements. Each element or 
+value that is inside of a list is called an item. Just as strings 
+are defined as characters between quotes, lists are defined by 
+having values between square brackets [ ]
+"""
 # List is linear data structure where we can store the homogeneous or non homogeneous element
 ls=[]           #-----Intializing the empty list
 
